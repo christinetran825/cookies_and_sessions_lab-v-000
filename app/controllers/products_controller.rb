fb97ed4b523b9c047a4e9a44989f71c71cf12d77
Christine Tran <christinetran825@gmail.com>
2017-11-21 23:28:10 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   def index
+    @products = cart
   end
 
   def add_to_cart
     cart << params[:product]
+    @products = cart
     render :index
   end
 
